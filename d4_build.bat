5884562d86369cd76391c9817a8f72d7b84de321
JordanCpp <Jordan631@yandex.ru>
2026-08-19 10:21:17 +0400

    Refactoring folders

--- d4_build.bat
+++ d4_build.bat
@@ -1,8 +1,10 @@
 @echo off
 cd /d D4
 
-for %%i in (..\*.pas) do (
-    dcc32 -Q -U..\Modules "%%i"
+for %%d in (..\Examples\, ..\Tests\) do (
+    for %%i in ("%%d*.pas") do (
+        dcc32 -Q -U..\Modules "%%i"
+    )
 )
 
 pause
--- fpc_build.bat
+++ fpc_build.bat
@@ -1,8 +1,9 @@
-
 @echo off
 
-for %%i in (*.pas) do (
-    C:\FPC\3.2.2\bin\i386-win32\fpc -FUBuild -FuModules "%%i"
+for %%d in (Examples Tests) do (
+    for %%i in (%%d\*.pas) do (
+        C:\FPC\3.2.2\bin\i386-win32\fpc -FUBuild -FuModules "%%i"
+    )
 )
 
 pause
