2007adff9954d8038c9c91eaa0cd18753135431a
JordanCpp <Jordan631@yandex.ru>
2026-08-28 09:27:20 +0400

    Refactoring examples

--- fpc32.bat
+++ fpc32.bat
@@ -1,6 +1,6 @@
 @echo off
 
-for %%d in (Examples Tests) do (
+for %%d in (Examples\OpenGL Examples\Painter Tests) do (
     for %%i in (%%d\*.pas) do (
         %1 -O3 -v0 -FUBuild -FuModules "%%i"
     )
